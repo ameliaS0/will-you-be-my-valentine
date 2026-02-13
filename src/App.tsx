8c570b5352118e133015baec29cbcf698eba8f36
amelia <amelia.saliev@cyberhack.fr>
2026-02-13 20:42:02 +0100

    color change

--- src/App.tsx
+++ src/App.tsx
@@ -35,27 +35,7 @@ export default function Page() {
   };
 
   return (
-    <div className="-mt-16 flex h-screen flex-col items-center justify-center relative overflow-hidden">
-      {/* Decorative hearts */}
-      <div className="absolute top-0 left-0">
-        <img src="https://i.pinimg.com/originals/1d/5e/cc/1d5ecc2339406e3c610bfe21a1aec1fb.gif" alt="heart" className="w-20 h-20 opacity-70" />
-      </div>
-      <div className="absolute top-0 right-0">
-        <img src="https://i.pinimg.com/originals/1d/5e/cc/1d5ecc2339406e3c610bfe21a1aec1fb.gif" alt="heart" className="w-20 h-20 opacity-70" />
-      </div>
-      <div className="absolute bottom-0 left-0">
-        <img src="https://i.pinimg.com/originals/1d/5e/cc/1d5ecc2339406e3c610bfe21a1aec1fb.gif" alt="heart" className="w-20 h-20 opacity-70" />
-      </div>
-      <div className="absolute bottom-0 right-0">
-        <img src="https://i.pinimg.com/originals/1d/5e/cc/1d5ecc2339406e3c610bfe21a1aec1fb.gif" alt="heart" className="w-20 h-20 opacity-70" />
-      </div>
-      <div className="absolute top-1/2 left-0 transform -translate-y-1/2 pointer-events-none">
-        <img src="https://i.pinimg.com/originals/1d/5e/cc/1d5ecc2339406e3c610bfe21a1aec1fb.gif" alt="heart" className="w-16 h-16 opacity-70" />
-      </div>
-      <div className="absolute top-1/2 right-0 transform -translate-y-1/2 pointer-events-none">
-        <img src="https://i.pinimg.com/originals/1d/5e/cc/1d5ecc2339406e3c610bfe21a1aec1fb.gif" alt="heart" className="w-16 h-16 opacity-70" />
-      </div>
-      
+    <div className="-mt-16 flex h-screen flex-col items-center justify-center relative overflow-hidden" style={{ backgroundColor: "#FFF0F5" }}>
       {yesPressed ? (
         <>
           <img src="https://i.pinimg.com/originals/1f/a2/2b/1fa22befc10e3cbacd58c5b407a97997.gif" />
